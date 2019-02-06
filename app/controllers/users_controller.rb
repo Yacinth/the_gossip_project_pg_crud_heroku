7b2e9cc2fa6f1a25e672ab309ac26b81bfa5e742
Yacinth <fevriercjjc@gmail.com>
2019-02-07 00:11:59 +0100

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   def show
-    id_params = params[:id_user].to_i
-    @array_user = User.all
-    @user = @array_user[id_params-1]
+    # id_params = params[:id_user].to_i
+    # @array_user = User.all
+    # @user = @array_user[id_params-1]
 
   end
 end
