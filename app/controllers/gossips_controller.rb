d921082ecba77d9c0febe89582881c8d5d22d218
Yacinth <fevriercjjc@gmail.com>
2019-02-06 23:53:37 +0100

    transfer code old app to CRUD app, controllers, views

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,8 +1,29 @@
 class GossipsController < ApplicationController
   def index
+    puts "$" * 60
+    puts "Voici le message de l'URL :"
+    puts  params[:first_name]
+    puts "$" * 60
+    @first_name = params[:first_name]
+    @author = User.first.first_name
+    @title = Gossip.first.title
+
+    @array_gossip = Gossip.all
   end
 
   def show
+    puts "$" * 60
+    puts params[:id_gossip]
+    puts "$" * 60
+    id_params_s = params[:id_gossip]
+    id_params = params[:id_gossip].to_i
+    @array_gossip = Gossip.all
+    @gossip = @array_gossip[id_params-1]
+    #@id_gossip = params[:id_gossip]
+    # @array_gossip = Gossip.all
+    # @id_gossip = Gossip.find(params[:id_gossip])
+    # @where_gossip = Gossip.where(id: params[:id_gossip])
+
   end
 
   def new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def show
+    id_params = params[:id_user].to_i
+    @array_user = User.all
+    @user = @array_user[id_params-1]
+
   end
 end
