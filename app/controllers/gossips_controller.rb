d346c22116fa381096c8e1fd008f0409e2e227bd
Yacinth <fevriercjjc@gmail.com>
2019-02-07 00:48:21 +0100

    controllers et views all good like monday on local

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,23 +1,23 @@
 class GossipsController < ApplicationController
   def index
-    # puts "$" * 60
-    # puts "Voici le message de l'URL :"
-    # puts  params[:first_name]
-    # puts "$" * 60
-    # @first_name = params[:first_name]
-    # @author = User.first.first_name
-    # @title = Gossip.first.title
+    puts "$" * 60
+    puts "Voici le message de l'URL :"
+    puts  params[:first_name]
+    puts "$" * 60
+    @first_name = params[:first_name]
+    @author = User.first.first_name
+    @title = Gossip.first.title
 
-    # @array_gossip = Gossip.all
+    @array_gossip = Gossip.all
   end
 
   def show
-    # puts "$" * 60
-    # puts params[:id_gossip]
-    # puts "$" * 60
-    # id_params = params[:id_gossip].to_i
-    # @array_gossip = Gossip.all
-    # @gossip = @array_gossip[id_params-1]
+    puts "$" * 60
+    puts params[:id]
+    puts "$" * 60
+    id_params = params[:id].to_i
+    @array_gossip = Gossip.all
+    @gossip = @array_gossip[id_params-1]
   end
 
   def new
