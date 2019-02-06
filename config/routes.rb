b026190f1219f74d59e2a784bf518aa2a9712eba
Yacinth <fevriercjjc@gmail.com>
2019-02-06 23:46:29 +0100

    roads gossips, user, root and their controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'gossips#index'
+  get 'static_pages/team'
+  get 'static_pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
+  resources :gossips, only: [:index,:show, :new, :create]
+
 end
